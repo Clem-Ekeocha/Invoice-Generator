e0cff4ca47c72d60807aeac41c941c354111392c
AccessClem <63481051+Clem-Ekeocha@users.noreply.github.com>
2024-05-26 00:05:11 +0100

    Add the header, total prices, company name and company logo.

--- main.py
+++ main.py
@@ -60,5 +60,25 @@ for filepath in filepaths:
         pdf.cell(w=30, h=8,txt=str(row["total_price"]), border=1, ln=1)
 
 
+    # Add a new horizontal line in the table to calculate the total_sum of "total_price"
+    total_sum = df["total_price"].sum()
+    pdf.set_font(family="Times", size=10)
+    pdf.set_text_color(80, 80, 80)
+    pdf.cell(w=30, h=8, txt="", border=1)
+    pdf.cell(w=60, h=8, txt="", border=1)
+    pdf.cell(w=40, h=8, txt="", border=1)
+    pdf.cell(w=30, h=8, txt="", border=1)
+    pdf.cell(w=30, h=8, txt=str(total_sum), border=1, ln=1)
+
+    # Add the total sum line sentence
+    pdf.set_font(family="Times", size=10, style='B')
+    pdf.cell(w=30, h=8, txt=f"The total price is ${total_sum}", ln=1)
+
+    # Add the company name and logo
+    pdf.set_font(family="Times", size=14, style='B')
+    pdf.cell(w=25, h=8, txt=f"AccessClem Analytics", ln=1)
+    pdf.image("img.png", w=10)
+
+
     pdf.output(f"Pdf_Output/{filename}_output.pdf")
 
